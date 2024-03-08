7251a29a78fd3091eb644e1cc19f760e8ea728a8
Johan george <johangeorge201203@gmail.com>
2024-03-09 00:07:01 +0530

    hehe

--- jo/app/(home)/components/Projects.tsx
+++ jo/app/(home)/components/Projects.tsx
@@ -3,6 +3,7 @@ import { SiCss3, SiHtml5, SiReact, SiTailwindcss } from "react-icons/si";
 import Title from "./Title";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { DirectionAwareHover } from "@/components/ui/direction-aware-hover";
 
 export default function Projects() {
   const projects = [
@@ -59,7 +60,13 @@ export default function Projects() {
         {projects.map((project, index) => {
           return (
             <Link href={project.link} key={index}>
-              <div className={cn("p-5 rounded-md", project.background)}></div>
+              <div className={cn("p-5 rounded-md", project.background)}>
+              <DirectionAwareHover
+              imageUrl={project.cover}
+              className="w-full space-y-5 cursor-pointer">
+                <h1>{project.title}</h1>
+              </DirectionAwareHover>
+              </div>
             </Link>
           );
         })}
--- jo/components/ui/direction-aware-hover.tsx
+++ jo/components/ui/direction-aware-hover.tsx
@@ -66,7 +66,7 @@ export const DirectionAwareHover = ({
       onMouseEnter={handleMouseEnter}
       ref={ref}
       className={cn(
-        "md:h-96 w-60 h-60 md:w-96 bg-transparent rounded-lg overflow-hidden group/card relative",
+        " bg-transparent rounded-lg overflow-hidden group/card relative",
         className
       )}
     >
