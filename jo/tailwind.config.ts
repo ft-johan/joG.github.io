76af2bc60c7e5dc81e3a2b5f7367bcb74f109466
Johan george <johangeorge201203@gmail.com>
2024-03-08 22:06:28 +0530

    h

--- jo/tailwind.config.ts
+++ jo/tailwind.config.ts
@@ -1,5 +1,13 @@
 import type { Config } from "tailwindcss"
 
+
+const svgToDataUri = require("mini-svg-data-uri");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config = {
   darkMode: ["class"],
   content: [
