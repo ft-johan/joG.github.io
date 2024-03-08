4c49dadcc2b91ee8d4293ed6ba605fafbc8243a1
Johan george <johangeorge201203@gmail.com>
2024-03-09 00:26:29 +0530

    komplete

--- jo/app/(home)/components/Navbar.tsx
+++ jo/app/(home)/components/Navbar.tsx
@@ -1,39 +1,39 @@
-import Link from 'next/link'
-import React from 'react'
-import { SiGithub, SiInstagram, SiLinkedin } from 'react-icons/si'
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+import { SiGithub, SiInstagram, SiLinkedin } from "react-icons/si";
 
-export default function Navbar() {
-    const socials = [
-        {
-            link : "",
-            label : "",
-            icon : SiGithub,
-        },
-        {
-            link : "",
-            label : "",
-            icon : SiLinkedin,
-        },
-        {
-            link : "",
-            label : "",
-            icon : SiInstagram,
-        },
-    ]
-  return (
-    <nav className="py-10 flex justify-between items-center">
-<h1 className="text-2xl font-bold">Johan George</h1>
-<div className="flex item-center gap-10">
-    {socials.map((social,index)=>
+export default function Navbar({className}:{className?:string}) {
+  const socials = [
     {
-        const Icon =social.icon;
-        return (
-        <Link href={social.link} key={index} aria-label= {social.label}>
-            <Icon className="w-5 h-10 hover:scale-125 transition-all" />
-        </Link>
-    );
-    })}
-</div>
+      link: "",
+      label: "",
+      icon: SiGithub,
+    },
+    {
+      link: "",
+      label: "",
+      icon: SiLinkedin,
+    },
+    {
+      link: "",
+      label: "",
+      icon: SiInstagram,
+    },
+  ];
+  return (
+    <nav className={cn("py-10 flex justify-between items-center",className)}>
+      <h1 className="text-2xl font-bold">Johan George</h1>
+      <div className="flex item-center gap-10">
+        {socials.map((social, index) => {
+          const Icon = social.icon;
+          return (
+            <Link href={social.link} key={index} aria-label={social.label}>
+              <Icon className="w-5 h-10 hover:scale-125 transition-all" />
+            </Link>
+          );
+        })}
+      </div>
     </nav>
-  )
+  );
 }
--- jo/app/(home)/page.tsx
+++ jo/app/(home)/page.tsx
@@ -3,10 +3,11 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-<div className="min-h-[400vh] bg-black overflow-hidden ">
+<div className="min-h-screen bg-black overflow-hidden ">
   <div className="dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative">
   <div className="max-w-7xl mx-auto p-5">
     <Navbar />
@@ -17,6 +18,7 @@ export default function Home() {
 <div className="max-w-7xl mx-auto p-5">
   <Skills />
   <Projects/>
+  <Footer/>
 </div>
 </div>
     );
