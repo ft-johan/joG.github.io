f93a4d9d427203f40fe0e7a319492d2c66d922ed
Johan george <johangeorge201203@gmail.com>
2024-03-08 20:28:07 +0530

    socials

--- jo/app/(home)/components/Navbar.tsx
+++ jo/app/(home)/components/Navbar.tsx
@@ -1,9 +1,34 @@
+import Link from 'next/link'
 import React from 'react'
+import { SiGithub, SiInstagram, SiLinkedin } from 'react-icons/si'
 
 export default function Navbar() {
+    const socials = [
+        {
+            link : "",
+            label : "",
+            icon : SiGithub,
+        },
+        {
+            link : "",
+            label : "",
+            icon : SiLinkedin,
+        },
+        {
+            link : "",
+            label : "",
+            icon : SiInstagram,
+        },
+    ]
   return (
     <nav>
-
+<h1>Johan George</h1>
+<div>
+    {socials.map((social)=>
+    {
+        return <Link></Link>
+    })}
+</div>
     </nav>
   )
 }
