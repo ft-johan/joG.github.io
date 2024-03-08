5fe793161d21907e2eb285f3114ca403b1c8113e
Johan george <johangeorge201203@gmail.com>
2024-03-08 23:43:09 +0530

    added png

--- jo/app/(home)/components/Projects.tsx
+++ jo/app/(home)/components/Projects.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { SiCss3, SiHtml5, SiReact, SiTailwindcss } from 'react-icons/si'
 
 export default function Projects() {
-  return (
+    const projects =[
+        {
+            title: "e learn",
+            tech: [SiReact,SiTailwindcss,SiHtml5,SiCss3],
+            link: "https",
+            cover: "",
+        }
+    ]
+    return (
+
     <div>k</div>
   )
 }
