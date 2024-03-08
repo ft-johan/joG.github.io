149f31bc5023b7f161c88037087883562abbe893
Johan george <johangeorge201203@gmail.com>
2024-03-08 20:14:20 +0530

    navbar start

---
+++ jo/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+<div className="min-h-screen bg-black ">
+  <div className="max-w-7xl mx-auto">
+    <Navbar />
+  </div>
+</div>
+    );
+}
