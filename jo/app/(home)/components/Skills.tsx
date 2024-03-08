318d6fb1ed8d96a49b39de13b78d98fbbe776e3d
Johan george <johangeorge201203@gmail.com>
2024-03-08 23:00:26 +0530

    s

--- jo/app/(home)/components/Skills.tsx
+++ jo/app/(home)/components/Skills.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Title from './Title'
+import { SiReact } from 'react-icons/si'
+import { HoverEffect } from '@/components/ui/card-hover-effect'
 
 export default function Skills() {
+    const skills = [
+        {
+            text: "React",
+            Icon: SiReact,
+        },
+        {
+            text: "React",
+            Icon: SiReact,
+        },
+    ]
   return (
+
     <div>
-        <Title text='Skills' className="flex flex-col items-center justify-center rotate-6"/> 
+        <Title text='Skills' className="flex flex-col items-center justify-center rotate-6"/>
+        <HoverEffect/> 
     </div>
   )
 }
