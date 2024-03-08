222532d8d786c638b72725b96b0d38feb8fca757
Johan george <johangeorge201203@gmail.com>
2024-03-08 21:49:42 +0530

    hero

--- jo/app/(home)/components/Hero.tsx
+++ jo/app/(home)/components/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <div className=" min-h-[80vh] flex items-center justify-between">
+    <div className=" min-h-[80vh] flex flex-col lg:flex-row items-center justify-between">
       <div className="space-y-10">
         <h1 className="text-4xl lg:text-7xl font-bold">
           Hi nice to meet you
@@ -11,19 +11,19 @@ export default function Hero() {
           I'm Johan
         </h1>
         <p className="md:w-96 text-lg text-gray-300">{"Based in India , I'm a Fullstack Developer a Btech Sophmore"}</p>
-        <Link href={"mailto:johangeorge201203@gmail.com"}>
-          <div>
-            <h1>Contact me</h1>
+        <Link href={"mailto:johangeorge201203@gmail.com"} className="inline-block">
+          <div >
+            <h1 className="text-3xl font-bold hover:text-teal-600 transition-all">Contact me</h1>
           </div>
         </Link>
       </div>
-      <div>
-        <div className="w-72 h-72 space-y-5">
+       <div>
+        <div className="w-72 h-72 space-y-3 -rotate-[30deg]">
           <div className="flex gap-3 translate-x-8">
             <div className="w-32 h-32 rounded-full bg-green-500"></div>
             <div className="w-32 h-32 rounded-2xl bg-indigo-500"></div>
           </div>
-          <div className="flex gap-3 translate-x-8">
+          <div className="flex gap-3 -translate-x-8">
             <div className="w-32 h-32 rounded-2xl bg-indigo-500"></div>
             <div className="w-32 h-32 rounded-full bg-green-500"></div>
           </div>
--- jo/app/(home)/page.tsx
+++ jo/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero";
 export default function Home() {
   return (
 <div className="min-h-screen bg-black ">
-  <div className="max-w-7xl mx-auto">
+  <div className="max-w-7xl mx-auto sm:p-5 overflow-hidden">
     <Navbar />
     <Hero />
   </div>
